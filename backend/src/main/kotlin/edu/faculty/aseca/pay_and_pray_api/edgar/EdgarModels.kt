25e1e383042639ddc04da1baafb2ce9f551c7cbf
fersantisi <fer.santisi@hotmail.com>
2026-05-14 03:51:08 -0300

    refactor: update application configuration and simplify model classes by removing JsonIgnoreProperties

--- backend/src/main/kotlin/edu/faculty/aseca/pay_and_pray_api/edgar/EdgarModels.kt
+++ backend/src/main/kotlin/edu/faculty/aseca/pay_and_pray_api/edgar/EdgarModels.kt
@@ -1,9 +1,7 @@
 package edu.faculty.aseca.pay_and_pray_api.edgar
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class CompanySubmissions(
     val cik: String,
     val name: String,
@@ -11,14 +9,12 @@ data class CompanySubmissions(
     val exchanges: List<String> = emptyList()
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class CompanyFacts(
     val cik: String,
     val entityName: String,
     val facts: Map<String, Any> = emptyMap()
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class CompanyConcept(
     val cik: String,
     val entityName: String,
@@ -26,25 +22,20 @@ data class CompanyConcept(
     val units: Map<String, Any> = emptyMap()
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class FullTextSearchResult(val hits: FullTextHits)
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class FullTextHits(
     val total: FullTextTotal,
     val hits: List<FullTextHit> = emptyList()
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class FullTextTotal(val value: Int, val relation: String)
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class FullTextHit(
     @JsonProperty("_id") val id: String,
     @JsonProperty("_source") val source: Map<String, Any> = emptyMap()
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class CompanyTicker(
     @JsonProperty("cik_str") val cikStr: Int,
     val name: String,
