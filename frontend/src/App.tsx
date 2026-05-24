69a274388658e78dd2411b4e45a1cac587d194fb
Julian Ritondale <jritondale@mail.austral.edu.ar>
2026-05-24 09:45:20 -0300

    fix: switch ticker details route to public

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './auth/AuthContext'
-import { GuestRoute, ProtectedRoute } from './auth/RouteGuards'
 import Navbar from './components/Navbar'
 import CompanyPage from './pages/CompanyPage'
 import Home from './pages/Home'
@@ -12,38 +11,10 @@ function Shell() {
     <div className="min-h-full flex flex-col">
       <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <GuestRoute>
-              <LoginPage />
-            </GuestRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <GuestRoute>
-              <RegisterPage />
-            </GuestRoute>
-          }
-        />
-        <Route
-          path="/companies/:ticker"
-          element={
-            <ProtectedRoute>
-              <CompanyPage />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/companies/:ticker" element={<CompanyPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
