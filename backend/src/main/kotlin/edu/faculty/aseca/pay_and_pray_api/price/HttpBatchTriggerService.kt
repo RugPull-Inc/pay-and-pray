1f2006938c21f9f046fab435d81a10ddd075f381
Juan Decoud <decoudjuanmanuel1@gmail.com>
2026-06-03 12:36:22 -0300

    fix(T58): add @Qualifier to resolve ambiguous RestTemplate injection

--- backend/src/main/kotlin/edu/faculty/aseca/pay_and_pray_api/price/HttpBatchTriggerService.kt
+++ backend/src/main/kotlin/edu/faculty/aseca/pay_and_pray_api/price/HttpBatchTriggerService.kt
@@ -1,5 +1,6 @@
 package edu.faculty.aseca.pay_and_pray_api.price
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
@@ -7,7 +8,7 @@ import org.springframework.web.client.RestTemplate
 @Service
 class HttpBatchTriggerService(
     @Value("\${batch.service-url}") private val serviceUrl: String,
-    private val restTemplate: RestTemplate,
+    @Qualifier("batchRestTemplate") private val restTemplate: RestTemplate,
 ) : BatchTriggerService {
     override fun trigger() {
         restTemplate.postForObject("$serviceUrl/trigger", null, Void::class.java)
